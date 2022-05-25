8780a5d39d8c7cedff54dc5334f72f3efbd5af40
zhongtiansheng <coverlove@foxmail.com>
2022-05-25 19:23:42 +0800

    leetcode 5

---
+++ Typescript/LeetCode/5.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: Cover
+ * @Date: 2022-05-25 10:34:04
+ * @LastEditTime: 2022-05-25 10:34:05
+ * @LastEditors: Cover
+ * @Description: 
+ * @FilePath: /Typescript/LeetCode/5.ts
+ * So do you want take a leap of faith or become an old man, filled with regret waiting to die alone ?
+ */
